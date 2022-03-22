d78951d4f5cccdf4538e2c141a3b207b35d52ada
HanlenGit <94447216+HanlenGit@users.noreply.github.com>
2022-03-22 20:33:38 +0800

    DRF commit
    Customer User

--- crowdfunding/users/models.py
+++ crowdfunding/users/models.py
@@ -4,14 +4,10 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 class CustomUser(AbstractUser):
     user_name = models.CharField(max_length=200)
-    is_supporter = 
-    is_donor = 
+    user_type = models.PositiveSmallIntegerField(choices='supporter', 'project_user')
     password = models.CharField(max_length=200)
     email = models.CharField(max_length=200)
     date_created = models.DateTimeField()
-
-
-
 
     def __str__(self):
         return self.username
