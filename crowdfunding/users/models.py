1cf1b617a874287fcce105581875a47d992c816d
HanlenGit <94447216+HanlenGit@users.noreply.github.com>
2022-05-04 19:39:06 +0800

    Update models.py customer user

--- crowdfunding/users/models.py
+++ crowdfunding/users/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class CustomUser(AbstractUser):
-    providing = models.ManyToManyField("projects.Service", related_name="providers", null=True, blank=True)
+    providing = models.ManyToManyField("projects.Service", related_name="providers")
 
     def __str__(self):
         return self.username
